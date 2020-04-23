dbff14458d5d9fe723255c85f2bd5f9ae1821975
MalikZone <ridwanmalik2010@gmail.com>
2020-04-23 17:04:14 +0700

    create method __construct in ChangePasswordControlle for middleware

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -11,6 +11,11 @@ use Illuminate\Support\Facades\Hash;
 class ChangePasswordController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(){
         return view('auth.passwords.change');
     }
